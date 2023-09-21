2b331f9d4ad044d881accb9be2239b3087fa3b63
open0072023 <124534410+open0072023@users.noreply.github.com>
2023-09-22 06:37:34 +0800

    Update mt7621.mk

--- config/1907/mt7621.mk
+++ config/1907/mt7621.mk
@@ -272,9 +272,7 @@ define Device/raisecom_msg1500-x-00
   DEVICE_VENDOR := RAISECOM
   DEVICE_MODEL := MSG1500
   DEVICE_VARIANT := X.00
-  DEVICE_PACKAGES := \
-	kmod-mt7615e kmod-usb3 kmod-usb-ledtrig-usbport wpad-basic \
-	uboot-envtools
+  DEVICE_PACKAGES := kmod-usb3 kmod-usb-ledtrig-usbport wpad-basic
 endef
 TARGET_DEVICES += raisecom_msg1500-x-00
 
